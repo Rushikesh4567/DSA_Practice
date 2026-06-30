e5b4090338a9d19841619d31c330fa8d8a4d9e8e
Rushikesh4567 <rushikeksh4567@gmail.com>
2026-06-30 07:10:30 +0530

    Solved array problem

---
+++ Day8/ReverseArray.java
@@ -0,0 +1,27 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter array Elements: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are: ");
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+}
