1322b3981c754d0fa851cd2c57343a7c4fd79be0
Rushikesh4567 <rushikeksh4567@gmail.com>
2026-07-04 09:00:35 +0530

    solved hollowsquare pattern

---
+++ Pattern_printing/HollowSquare.java
@@ -0,0 +1,28 @@
+package Pattern_printing;
+
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void printArray(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter row size: ");
+        int n = sc.nextInt();
+
+        printArray(n);
+    }
+}
