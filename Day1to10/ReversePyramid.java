250d2f264da319f58f6816aeab36fa85a1f57cd0
Rushikesh4567 <rushikeksh4567@gmail.com>
2026-07-12 11:26:33 +0530

    added reverse pyramid

---
+++ Day1to10/ReversePyramid.java
@@ -0,0 +1,26 @@
+package Day1to10;
+
+import java.util.Scanner;
+
+public class ReversePyramid {
+
+    public static void printArray(int n) {
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter row size: ");
+        int n = sc.nextInt();
+
+        printArray(n);
+    }
+}
