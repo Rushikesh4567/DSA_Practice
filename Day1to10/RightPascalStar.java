bda9b36bd3f9c2d00a00756cb53a6b0bc64a8263
Rushikesh4567 <rushikeksh4567@gmail.com>
2026-07-17 20:55:38 +0530

    solved pattern question

---
+++ Day1to10/RightPascalStar.java
@@ -0,0 +1,33 @@
+package Day1to10;
+
+import java.util.Scanner;
+
+public class RightPascalStar {
+    public static void printArray(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter row size: ");
+        int n = sc.nextInt();
+
+        printArray(n);
+    }
+}
