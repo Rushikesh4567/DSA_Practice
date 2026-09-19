97169720b05e10d73d342997f2fc04602ff37623
Rushikesh4567 <rushikeksh4567@gmail.com>
2026-09-19 11:11:23 +0530

    Added the folder for solving string and recursion

---
+++ Day21to30/Traversal/CountCharacters.java
@@ -0,0 +1,12 @@
+package Day21to30.Traversal;
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        String s = "HELLO";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            System.out.println(ch + " ");
+        }
+    }
+}
